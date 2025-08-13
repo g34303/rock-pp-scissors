5add4dda587acf57944299d610ad3786b46c05b2
Stephanie Leung <leung.stephanie.j@gmail.com>
2025-08-12 19:09:22 -0700

    added prescreen and fixed buttons behavior

--- script.js
+++ script.js
@@ -1,5 +1,69 @@
-//for the timer
+// function to hide elements
+function removeElement(element) {
+const tm = document.getElementById(element);
+tm.style.visibility = "hidden";
+tm.style.opacity = "0";
+}
 
+// start timer and hide prescreen
+function startGame() {
+  removeElement("prescreen");
+  document.getElementById("mainscreen").style.display = "";
+  startTimer();
+}
+
+// hide prescreen shortcut
+let keyCount = 0;
+const targetKey = "`";
+const triggerCount = 3; 
+
+document.addEventListener("keydown", (event) => {
+  if (event.key === targetKey) {
+    keyCount++;
+    if (keyCount >= triggerCount) {
+    startGame();
+  }
+  }
+});
+
+//come up with a username for p2
+// const usernamePartOne = ["ominous", "demure", "verdant", "resolute", "zesty", "eerie", "perky", "opulent"];
+// const usernamePartTwo = ["bird", "dog", "cat", "chicken", "pig", "mongoose", "worm"];
+
+const username = ["ialwayspickpaper", "BlazeIt420", "b00baLuver69", "stinkyUwU", "sigmafreud", "norman", "bigchVngus"];
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+//const player2name = usernamePartOne[getRandomIntInclusive(0,usernamePartOne.length - 1)] + usernamePartTwo[getRandomIntInclusive(0,usernamePartTwo.length - 1)] + getRandomIntInclusive(0,9) + getRandomIntInclusive(0,9) + getRandomIntInclusive(0,9);
+
+const player2name = username[getRandomIntInclusive(0,username.length - 1)];
+
+document.getElementById("player2name").innerText = player2name;
+
+
+// enables start-game button if user inputs their name
+document.getElementById("name-input").addEventListener("input", () => {
+  if (document.getElementById("name-input").value.trim() !== "") {
+    document.getElementById("start-game").removeAttribute("disabled");
+    console.log("user entered something");
+  } else {
+    document.getElementById("start-game").setAttribute("disabled","");
+    console.log("input is blank")
+  }
+});
+
+document.getElementById("start-game").addEventListener("click", startGame);
+document.getElementById("name-input").addEventListener("keydown", (key) => {
+  if (key.key === "Enter" && document.getElementById("name-input").value.trim() !== "") {
+    startGame();
+  }
+});
+
+//for the timer
 const FULL_DASH_ARRAY = 283;
 const WARNING_THRESHOLD = 3;
 const ALERT_THRESHOLD = 1;
@@ -18,13 +82,14 @@ const COLOR_CODES = {
   }
 };
 
-const TIME_LIMIT = 5;
+const TIME_LIMIT = 10;
 let timePassed = 0;
 let timeLeft = TIME_LIMIT;
 let timerInterval = null;
 let remainingPathColor = COLOR_CODES.info.color;
 
 document.getElementById("timer").innerHTML = `
+<h2> Choose your hand</h2>
 <div class="base-timer">
   <svg class="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
     <g class="base-timer__circle">
@@ -48,8 +113,6 @@ document.getElementById("timer").innerHTML = `
 </div>
 `;
 
-startTimer();
-
 function onTimesUp() {
   clearInterval(timerInterval);
 }
@@ -66,6 +129,7 @@ function startTimer() {
 
     if (timeLeft === 0) {
       onTimesUp();
+      setTimeout(removeElement("timer"), 500);
     }
   }, 1000);
 }
@@ -135,8 +199,7 @@ function setCircleDasharray() {
         });
 
         transitions++;
-
-        //console.log(transitions);
+        
     }
 
     setTimeout(fadeImage, 1000);
@@ -150,11 +213,13 @@ function setCircleDasharray() {
         setTimeout(fadeImage, 1000);
         }
     }
-    }); // closing bracket for code to cycle through the pictures
+    });
 
+// click on a hand to make it active
 document.querySelectorAll("#p1-buttons button").forEach(btn => {
    btn.addEventListener("click", () => {
     document.querySelectorAll("#p1-buttons button").forEach(b => b.classList.remove("active"));
     btn.classList.add('active');
+    document.getElementById("confirm-button").removeAttribute("disabled");
     }); 
 });
